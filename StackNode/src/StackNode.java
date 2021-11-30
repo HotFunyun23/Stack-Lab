56afe4579533d1a73a454e5cfe0375fc54ffe8b1
HotFunyun23 <cschaden22@kentdenver.org>
2021-11-30 11:06:50 -0700

    comments

--- StackNode/src/StackNode.java
+++ StackNode/src/StackNode.java
@@ -5,19 +5,21 @@
  **/
 public class StackNode<E> {
 
-
+//instance data
     private StackNode<E> next;
     private E element;
 
+    //pushes element to top
     public void push(E element){
         next = new StackNode<E>();
         this.element = element;
 
     }
+    // empty
     public boolean isEmpty(){
         return element == null;
     }
-    
+    //if empty tells and gives element
     public E pop(){
         if(next.isEmpty){
             System.out.println("Stack is Empty");
@@ -28,11 +30,11 @@ public class StackNode<E> {
 
         return top;
     }
-
+//gives size
     public int size(){
         return this.isEmpty() ? 0 : 1 + this.next.size(); //recursion?? lets see if it works
     }
-
+//gives top
     public E peek(){
         return element;
     }
